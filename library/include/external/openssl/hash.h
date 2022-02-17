17fa347b4404c40654bb028996bd001b685b99d3
unrealinsanity <rob3rt.233@gmail.com>
2022-02-17 19:04:57 +0800

    [LIB] Throw Crypto++ away
    ... in favour of OpenSSL, it sucks but it sucks less

---
+++ library/include/external/openssl/hash.h
@@ -0,0 +1,13 @@
+/**
+ * @file external/openssl/hash.h
+ * @brief Enable OpenSSL hash functions.
+ * @copyright Copyleft 2022 "unrealinsanity". All rights reversed.
+ */
+
+#ifndef EXTERNAL_OPENSSL_HASH_INCLUDED
+#define EXTERNAL_OPENSSL_HASH_INCLUDED
+
+#include <openssl/md5.h>
+#include <openssl/sha.h>
+
+#endif
---
+++ library/include/own/generic/hash.h
@@ -0,0 +1,121 @@
+/**
+ * @file own/generic/hash.h
+ * @brief Hashing with ease.
+ * @copyright Copyleft 2022 "unrealinsanity". All rights reversed.
+ */
+
+#ifndef OWN_GENERIC_COMPUTE_HASH_INCLUDED
+#define OWN_GENERIC_COMPUTE_HASH_INCLUDED
+
+#include <external/openssl/hash.h>
+#include <own/generic/support.h>
+
+namespace own::hash
+{
+
+class Hash final
+{
+    STATIC_CLASS(Hash)
+
+   public:
+    /// Indicates whether hashes should be lowercase or uppercase
+    enum class NeedLowerCase : bool
+    {
+        No,  ///< Hash would be in lowercase
+        Yes  ///< Hash would be in uppercase
+    };
+
+   private:
+    /** Make printable hash from binary hash
+     *
+     * @param rawHash is the binary hash
+     * @param needLower indicates if the output should be lower cased
+     * @returns the printable hash
+     */
+    [[nodiscard]] static inline auto makePrintableHash(const std::span<const u_char> rawHash,
+                                                       const Hash::NeedLowerCase needLower)
+    {
+        std::string result;
+        result.resize(rawHash.size() * 2);
+        for (size_t index = 0; index < rawHash.size(); ++index)
+        {
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg, hicpp-vararg)
+            sprintf(&result.at(index * 2), "%x", rawHash[index]);
+        }
+        return (needLower == NeedLowerCase::Yes) ? pystring::lower(result)
+                                                 : pystring::upper(result);
+    }
+
+   public:
+    /** Compute MD5 hash
+     *
+     * @param message is the message to be hashed
+     * @param needLower indicates if the output should be lower cased
+     * @returns the hashed message
+     */
+    [[nodiscard]] static inline auto computeMd5Hash(const std::string_view message,
+                                                    const Hash::NeedLowerCase needLower) noexcept
+    {
+        std::vector<u_char> result;
+        result.resize(MD5_DIGEST_LENGTH);
+
+/// @todo Take other compiler extension into account
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        MD5(
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+            reinterpret_cast<const u_char* const>(message.data()), message.size(), result.data());
+#pragma clang diagnostic pop
+
+        return makePrintableHash(result, needLower);
+    }
+
+    /** Compute SHA256 hash
+     *
+     * @param message is the message to be hashed
+     * @param needLower indicates if the output should be lower cased
+     * @returns the hashed message
+     */
+    [[nodiscard]] static inline auto computeSha256Hash(const std::string_view message,
+                                                       const Hash::NeedLowerCase needLower) noexcept
+    {
+        std::vector<u_char> result;
+        result.resize(SHA256_DIGEST_LENGTH);
+
+        SHA256(
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+            reinterpret_cast<const u_char* const>(message.data()), message.size(), result.data());
+
+        return makePrintableHash(result, needLower);
+    }
+};
+
+/** Compute MD5 hash
+ *
+ * @param message is the message to be hashed
+ * @param needLower indicates if the output should be lower cased
+ * @returns the hashed message
+ */
+[[nodiscard]] inline auto computeMd5Hash(
+    const std::string_view message,
+    const Hash::NeedLowerCase needLower = Hash::NeedLowerCase::Yes) noexcept
+{
+    return Hash::computeMd5Hash(message, needLower);
+}
+
+/** Compute SHA256 hash
+ *
+ * @param message is the message to be hashed
+ * @param needLower indicates if the output should be lower cased
+ * @returns the hashed message
+ */
+[[nodiscard]] inline auto computeSha256Hash(
+    const std::string_view message,
+    const Hash::NeedLowerCase needLower = Hash::NeedLowerCase::Yes) noexcept
+{
+    return Hash::computeSha256Hash(message, needLower);
+}
+
+}  // namespace own::hash
+
+#endif
